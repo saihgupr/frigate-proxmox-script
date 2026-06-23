2bfa1779d60d36cd7ff90015085c37cc8384c1a8
Kyle Ekstrand <KSEkstrand@gmail.com>
2026-06-23 10:39:37 -0800

    Update install.sh
    Modified check to use getent to deb.debian.org

--- install.sh
+++ install.sh
@@ -1381,7 +1381,7 @@ start_lxc_container() {
         log "Waiting for container to initialize network..."
         local counter=0
         while [ $counter -lt 30 ]; do
-            if pct exec "$CT_ID" -- wget --spider -S ifconfig.io 2>&1 | grep "connected" > /dev/null; then
+            if pct exec "$CT_ID" -- getent deb.debian.org 2>&1 | grep "deb.debian.org" > /dev/null; then
                 break
             fi
             sleep 1
